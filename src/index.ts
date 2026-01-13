13c663493cb17b2223dc34855976b4f7a56ae52a
Pe-ter-M <peterwahu3491@gmail.com>
2026-01-13 13:39:42 +0300

    swagger start

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { transactions_route } from './transactions/transactions.route.js'
+import { swaggerUI } from '@hono/swagger-ui'
+import { openapiSpec } from './openapispec.js'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+// Add Swagger UI endpoint
+app.get('/swagger', swaggerUI({ url: '/openapi' }))
+
+// Add OpenAPI JSON endpoint
+app.get('/openapi', (c) => c.json(openapiSpec))
+
+app.route('/transactions', transactions_route)
 
 serve({
   fetch: app.fetch,
